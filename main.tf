a11c588415842bdd0b48b6dc3def1b389ee779aa
tropicalmentat <gabtorres1011@gmail.com>
2021-04-16 22:59:37 +0000

    First commit

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider digitalocean {
+    token = var.do_token
+    version = "2.7.0"
+}
+
+variable do_token {}
+
+resource "digitalocean_project" "sandbox" {
+   name="sandbox"
+   resources=[digitalocean_vpc.web.urn,digitalocean_droplet.gateway.urn,digitalocean_droplet.api.urn,digitalocean_droplet.gh.urn,digitalocean_droplet.db.urn]
+}
+
+resource "digitalocean_droplet" "gateway" {
+   name="rowt-dev-db"
+   image="ubuntu-20-04-x64"
+   region="sgp1"
+   size="s-1vcpu-1gb"
+}
+
+resource "digitalocean_droplet" "api" {
+   name="rowt-dev-api"
+   image="ubuntu-20-04-x64"
+   region="sgp1"
+   size="s-1vcpu-1gb"
+}
+
+resource "digitalocean_vpc" "web" {
+   name="rowt-dev-vpc"
+   region="sgp1"
+   ip_range="10.104.16.0/20"
+}
+
+resource "digitalocean_droplet" "gh" {
+   name="rowt-dev-gh"
+   image="ubuntu-20-04-x64"
+   region="sgp1"
+   size="s-2vcpu-4gb"
+}
+
+resource "digitalocean_droplet" "db" {
+   name="rowt-dev-db"
+   image="ubuntu-20-04-x64"
+   region="sgp1"
+   size="s-1vcpu-1gb"
+}
